15043087957cb922a8b8377612f7aae7e0db030b
DJ_Kovrik <djkovrik@gmail.com>
2017-03-07 13:02:22 +0300

    Stetho added.

--- app/src/main/java/com/udacity/stockhawk/StockHawkApp.java
+++ app/src/main/java/com/udacity/stockhawk/StockHawkApp.java
@@ -2,6 +2,8 @@ package com.udacity.stockhawk;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
+
 import timber.log.Timber;
 
 public class StockHawkApp extends Application {
@@ -13,6 +15,8 @@ public class StockHawkApp extends Application {
         if (BuildConfig.DEBUG) {
             Timber.uprootAll();
             Timber.plant(new Timber.DebugTree());
+
+            Stetho.initializeWithDefaults(this);
         }
     }
 }
